8cf06a54eba7c48df1e2628ab51092315320e31a
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2021-06-03 22:46:08 +0200

    order - partial cleanup

--- order/src/Infrastructure/Framework/Symfony/Kernel.php
+++ order/src/Infrastructure/Framework/Symfony/Kernel.php
@@ -1,7 +1,7 @@
 <?php 
 declare(strict_types=1);
 
-namespace App;
+namespace App\Infrastructure\Framework\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
